6b9d0a23453904b0653797136dc94bbbfb0aca81
Daniel Kwolek <Daniel@Daniels-MacBook-Pro.local>
2016-09-08 21:23:41 -0400

    here is a starter for the creepy smiley man

---
+++ Smiley man/FaceViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Smiley man
+//
+//  Created by Daniel Kwolek on 9/8/16.
+//  Copyright © 2016 Arcore. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+    
+    
+
+}
+
